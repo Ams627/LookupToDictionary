f49c9f7974cbea5ebbee2e12753022f2048306d9
Ams <adriansims62781@gmail.com>
2017-07-30 11:41:29 +0100

    Add project files.

---
+++ LookupToDictionary/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace LookupToDictionary
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            try
+            {
+                var doc = new XDocument(
+                        new XDeclaration("1.0", "utf-8", "no"),
+            )
+            }
+            catch (Exception ex)
+            {
+                var codeBase = System.Reflection.Assembly.GetExecutingAssembly().CodeBase;
+                var progname = Path.GetFileNameWithoutExtension(codeBase);
+                Console.Error.WriteLine(progname + ": Error: " + ex.Message);
+            }
+
+        }
+    }
+}
---
+++ gitmehappy.cmd
@@ -0,0 +1,8 @@
+rem echo off
+git init
+git add LookupToDictionary/Program.cs
+git add LookupToDictionary/App.config
+git add LookupToDictionary/Properties/AssemblyInfo.cs
+git add LookupToDictionary/LookupToDictionary.csproj
+git add LookupToDictionary.sln
+git add .gitignore
